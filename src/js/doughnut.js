56443cdf48e9e256a9aa9500a743340e927d0c26
yura.l <yura.l@webcodium.com>
2015-11-27 21:18:35 +0200

    take off bad effect when resize

--- src/js/doughnut.js
+++ src/js/doughnut.js
@@ -67,10 +67,12 @@ angular
                             );
                         }
                     }
-                    $animateCss(element.find('circle').eq(1), {
-                        addClass: resize ? undefined : 'doughnut-allow-animation',
-                        to: {'stroke-dashoffset': service.getPercent(scope.percentage, scope.length)}
-                    }).start();
+                    scope.dashOffset = service.getPercent(scope.percentage, scope.length);
+                    if (resize) {
+                        scope.$apply();
+                    } else {
+                        element.find('circle').eq(1).addClass('doughnut-allow-animation');
+                    }
                 }
 
                 function firstAnimate() {
@@ -83,22 +85,19 @@ angular
                 function drawDoughNut(resize) {
                     scope.radius = (document.querySelectorAll('.' + scope.class)[0].offsetWidth - scope.stroke) / 2;
                     scope.length = service.getLengthCircle(scope.radius);
-                    $animateCss(element.find('circle').eq(1), {
-                        to: {'stroke-dashoffset': scope.length}
-                    }).start().then(function () {
-                        scope.drawed = true;
-                        //set cx, cy and r for circles
-                        scope.circles = element.find('circle').attr(
-                            service.setCircleAttrs(scope.radius, scope.stroke)
-                        );
-                        if (resize) {
-                            setDashOffset(true);
-                        }
-                        //for animation on load
-                        if (scope.percentage && !scope.animate) {
-                            firstAnimate();
-                        }
-                    });
+                    scope.dashOffset = scope.length;
+                    scope.drawed = true;
+                    //set cx, cy and r for circles
+                    scope.circles = element.find('circle').attr(
+                        service.setCircleAttrs(scope.radius, scope.stroke)
+                    );
+                    if (resize) {
+                        setDashOffset(true);
+                    }
+                    //for animation on load
+                    if (scope.percentage && !scope.animate) {
+                        firstAnimate();
+                    }
                 }
 
                 scope.class = service.getClass();
@@ -130,7 +129,7 @@ angular
             '<div class="dough-text-suffix"><span class="dough-text">{{curPercent}}</span><sup class="dough-suffix" ng-show="curPercent!=undefined">%</sup></div>' +
             '<svg xmlns="http://www.w3.org/2000/svg">' +
             '<circle fill="none" class="circle-bg" stroke-width="{{stroke}}"/>' +
-            '<circle fill="none" class="circle-animation" stroke-width="{{stroke}}" style="stroke-dasharray: {{length}};"/>' +
+            '<circle fill="none" class="circle-animation" stroke-width="{{stroke}}" style="stroke-dasharray: {{length}};stroke-dashoffset: {{dashOffset}}"/>' +
             '</svg>' +
             '</div>'
         });
